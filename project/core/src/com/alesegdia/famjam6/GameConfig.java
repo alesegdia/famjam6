60f0ae587f55dc104e08e770ab62634c811c3760
Alejandro Seguí <alesegdia@gmail.com>
2016-04-02 03:19:30 +0200

    resolves #7

--- project/core/src/com/alesegdia/famjam6/GameConfig.java
+++ project/core/src/com/alesegdia/famjam6/GameConfig.java
@@ -6,15 +6,11 @@ public class GameConfig {
 	public static final int WINDOW_HEIGHT = 600;
 	
 	public static final float RATIO = ((float)WINDOW_WIDTH) / ((float)WINDOW_HEIGHT);
-	public static final float SIZE = 128f;
+	public static final float SIZE = 64f + 32f + 16f;
 	public static final float VIEWPORT_WIDTH = SIZE * 1.5f;
 	public static final float VIEWPORT_HEIGHT = VIEWPORT_WIDTH / RATIO;
 	
 	public static final float METERS_TO_PIXELS = 16f;
 	public static final float PIXELS_TO_METERS = 1f / METERS_TO_PIXELS;
-	
-	public static final float BLOCK_X = 12f;
-	public static final float BLOCK_Y = 10f;
-	
-	
+
 }
