ba1c5eb4f5267293c3491f89271b3faa64193db0
Alejandro Seguí <alesegdia@gmail.com>
2016-04-02 02:07:46 +0200

    loading assets

---
+++ project/core/src/com/alesegdia/famjam6/asset/Gfx.java
@@ -0,0 +1,54 @@
+package com.alesegdia.famjam6.asset;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Gfx {
+
+	public static Spritesheet tileset;
+	public static TextureRegion froncetiteGathererTr;
+	public static TextureRegion sandetiteGathererTr;
+	public static TextureRegion baseExtensionTr;
+	public static TextureRegion powerPlantTr;
+	public static TextureRegion[] froncetiteTransportTr;
+	public static TextureRegion[] sandetiteTransportTr;
+	public static TextureRegion[] powerTransportTr;
+	
+	public static TextureRegion froncetiteTerrainTr;
+	public static TextureRegion sandetiteTerrainTr;
+	public static TextureRegion lowTerrainTr;
+	public static TextureRegion highTerrainTr;
+	
+	
+	public static void Initialize()
+	{
+		tileset = new Spritesheet("tileset5x4.png", 4, 5);
+		froncetiteGathererTr = tileset.get(4);
+		sandetiteGathererTr = tileset.get(8);
+		baseExtensionTr = tileset.get(14);
+		powerPlantTr = tileset.get(18);
+
+		// transports
+		froncetiteTransportTr = new TextureRegion[3];
+		sandetiteTransportTr = new TextureRegion[3];
+		powerTransportTr = new TextureRegion[3];
+		
+		loadTransport( 1, froncetiteTransportTr );
+		loadTransport( 2, sandetiteTransportTr );
+		loadTransport( 3, powerTransportTr );
+		
+		froncetiteTerrainTr = tileset.get(0);
+		sandetiteTerrainTr = tileset.get(1);
+		highTerrainTr = tileset.get(2);
+		lowTerrainTr = tileset.get(3);
+		
+	}
+
+
+	private static void loadTransport(int y_offset, TextureRegion[] trtr) {
+		for( int i = 0; i < trtr.length; i++ )
+		{
+			trtr[i] = tileset.get(y_offset * 5 + i);
+		}
+	}
+	
+}
