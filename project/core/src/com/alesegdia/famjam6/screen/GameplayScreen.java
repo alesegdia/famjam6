2b5d2d542a9b5074ab1258e1788cecc8c9de2f57
Alejandro Seguí <alesegdia@gmail.com>
2016-04-02 04:25:26 +0200

    adjusting pointer position

--- project/core/src/com/alesegdia/famjam6/screen/GameplayScreen.java
+++ project/core/src/com/alesegdia/famjam6/screen/GameplayScreen.java
@@ -166,7 +166,7 @@ public class GameplayScreen implements Screen {
 			float x, y;
 			x = Gdx.input.getX();
 			y = Gdx.input.getY();
-			Vector3 v = g.cam.unproject(new Vector3(x, y, 0));
+			Vector3 v = g.cam.unproject(new Vector3(x + 18, y - 20, 0));
 			scenario.tryApplyTool(v.x, v.y, this.currentTool);
 		}
 	}
