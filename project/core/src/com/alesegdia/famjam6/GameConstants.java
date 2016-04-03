ef3e80f1b96c63122dac1459d82f2705ecc6490a
Alejandro Seguí <alesegdia@gmail.com>
2016-04-03 15:23:16 +0200

    tweaking game constants

--- project/core/src/com/alesegdia/famjam6/GameConstants.java
+++ project/core/src/com/alesegdia/famjam6/GameConstants.java
@@ -8,7 +8,7 @@ public class GameConstants {
 	public static final float FG_F_COST = 5;
 	public static final float FG_S_COST = 5;
 	public static final float FG_PRODUCTION_RATE = 0.5f;
-	public static final float FG_POWER_CONSUMPTION_RATE = 3;
+	public static final float FG_POWER_CONSUMPTION_RATE = 5;
 
 	// froncetite transport
 	public static final float FT_S_COST = 1;
@@ -18,29 +18,35 @@ public class GameConstants {
 	public static final float SG_F_COST = 5;
 	public static final float SG_S_COST = 5;
 	public static final float SG_PRODUCTION_RATE = 0.5f;
-	public static final float SG_POWER_CONSUMPTION_RATE = 3;
+	public static final float SG_POWER_CONSUMPTION_RATE = 5;
 
 	// sandetite transport
 	public static final float ST_S_COST = 2;
 	public static final float ST_F_COST = 1;
 
 	// power plant ****************************************
-	public static final float PG_F_COST = 0;
-	public static final float PG_S_COST = 0;
-	public static final float PG_PRODUCTION_RATE = 5f;
-	public static final float PG_POWER_CONSUMPTION_RATE = 0;
+	public static final float PG_F_COST = 10;
+	public static final float PG_S_COST = 10;
+	public static final float PG_PRODUCTION_RATE = 8000f;
+	public static final float PG_POWER_CONSUMPTION_RATE = 0f;
 
 	// power transport
 	public static final float PT_F_COST = 2;
 	public static final float PT_S_COST = 2;
 
 	// generic ********************************************
-	public static final float TRANSPORT_CONSUMPTION_RATE = 0.05f;
+	public static final float TRANSPORT_CONSUMPTION_RATE = 1;
 	
 	// player stats
 	public static final float INITIAL_FRONCETITE = 50f;
 	public static final float INITIAL_SANDETITE = 50f;
 
-	public static final float POWER_MAX = 1000;
+	public static final float POWER_MAX = 10;
+
+	public static final float BASE_F_COST = 10;
+
+	public static final float BASE_S_COST = 10;
+
+	public static final float BASE_ECR = 5;
 
 }
