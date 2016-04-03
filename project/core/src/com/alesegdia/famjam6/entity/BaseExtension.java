100775fec39f99dbe7b824e06d084656560b4137
Alejandro Seguí <alesegdia@gmail.com>
2016-04-03 15:25:20 +0200

    using game constants for base costs

--- project/core/src/com/alesegdia/famjam6/entity/BaseExtension.java
+++ project/core/src/com/alesegdia/famjam6/entity/BaseExtension.java
@@ -1,9 +1,11 @@
 package com.alesegdia.famjam6.entity;
 
+import com.alesegdia.famjam6.GameConstants;
+
 public class BaseExtension extends Building {
 
 	public BaseExtension() {
-		super(10, 10, 5);
+		super(GameConstants.BASE_F_COST, GameConstants.BASE_S_COST, GameConstants.BASE_ECR);
 		// TODO Auto-generated constructor stub
 	}
 
